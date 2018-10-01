a4eeb3996efddf297094b63ed73ebae038b97ec6
Rustam4457 <punk60826@gmail.com>
2018-10-01 23:06:19 +0300

    navbar changing

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Магазин Rozetka</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="assets/css/style.css">
@@ -11,34 +11,20 @@
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 </head>
 <body>
-	 <nav class="navbar navbar-expand-sm navbar-dark bg-dark p-0">
-	 	    <div class="phones text-white">
-         <i class="fa fa-phone"></i> (333) 333-333-2222
-       </div>
-	 	  <a href="index.php" class="navbar-brand mr-3">
-        <img src="assets/img/192.png" alt="logo" title="Интернет магазин Rozetka">
-      </a>
+	 <nav>	 
     <div class="container">
-      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-       <form class="form-inline ">
-        <i class="fa fa-search" aria-hidden="true"></i>
-    <input class="form-control-lg  mr-3 " type="search" placeholder="Поиск" aria-label="Search">
+         <div class="phones mb-2">
+          <i class="fa fa-phone fa-lg "></i> 
+         (333) 333-333-2222,
+         (321) 32320-03 545-46
+         (334) 2345-43-3542
+   </div>
+      <a href="index.php" class="navbar-brand ml-7">
+        <img src="assets/img/192.png" class="logo" alt="logo" title="logo">    
+      </a>
+    <i class="fa fa-search" aria-hidden="true"></i>
+    <input type="search" class="form-searching" placeholder="Поиск" aria-label="Search">
     <button class="btn btn-success" type="submit">Поиск</button>
-  </form>
-      </div>
-        <ul class="navbar-nav navbar-right ml-auto">
-          <li class="nav-item mt-2">
-          	Здравствуйте,
-          </li>
-          <li class="nav-item" id="enter-register">
-            <a href="register.php" s class="nav-link">
-              <i class="fa fa-user"></i> войдите в личный кабинет
-            </a>
-          </li>
-        </ul>
     </div>
   </nav>
 </body>
