323c8e5e6f3bc443a94636614b4f1b623a0c79bb
Rustam4457 <punk60826@gmail.com>
2018-10-06 22:25:11 +0300

    links nav-top-menu under input[search] and carousel 3D added

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var carousel = $("#carousel").flipster({
+        style: 'carousel',
+        spacing: -0.5,
+        buttons:   true,
+    });
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
 	<title>Магазин Rozetka</title>
@@ -9,6 +9,7 @@
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
   <link rel="icon" type="image/ic" href="assets/img/favicon.ico">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/jquery.flipster.min.css">
 </head>
 <body>
 	 <nav>	 
@@ -26,12 +27,63 @@
       <a href="index.php" class="navbar-brand ml-7">
         <img src="assets/img/192.png" class="logo" alt="logo" title="logo">    
       </a>
+      <ul class="nav-top-menu">
+        <li class="top-list">
+          <a href="#!">Вопросы и ответы</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Кредит</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Доставка и оплата</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Гарантия</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Контакты</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Отследить заказ</a>
+        </li>
+        <li class="top-list">
+          <a href="#!">Продавать на Розетке</a>
+        </li>
+      </ul>
 
     <i class="fa fa-search fa-lg" aria-hidden="true"></i>
     <input type="search" class="searching-form" placeholder="Поиск" aria-label="Search">
     <button class="btn-search" type="submit">Поиск</button>
-
     </div>
   </nav>
+<div id="carousel">
+        <ul >
+            <li >
+                <img src="assets/img/1.jpg">
+            </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+             </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+            </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+            </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+            </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+            </li>
+            <li>
+                <img  src="assets/img/1.jpg">
+            </li>
+        </ul>
+    </div>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.flipster.min.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/script.js"></script>
 </body>
 </html>
